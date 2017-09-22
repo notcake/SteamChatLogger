1fdc3c8f34471388ea02eb3c1ecdf6d9b4148f09
notcake <cakenotfound@googlemail.com>
2017-09-22 16:57:15 +0100

    Added missing callback structs.

---
+++ Steam/Callbacks/AppLifetimeNotice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam.Callbacks
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+    public struct AppLifetimeNotice : ICallbackData
+    {
+        public const CallbackType k_iCallback = CallbackType.AppLifetimeNotice;
+
+        public AppId m_nAppID;
+        public int m_nInstanceID;
+        public bool m_bExiting;
+
+        #region ICallbackData
+        public CallbackType Type => k_iCallback;
+        #endregion
+    }
+}
---
+++ Steam/Callbacks/UserAchievementIconFetched.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam.Callbacks
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+    public struct UserAchievementIconFetched : ICallbackData
+    {
+        public const CallbackType k_iCallback = CallbackType.UserAchievementIconFetched;
+
+        public CGameID m_nGameID;              // Game this is for
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 128)]
+        public byte[] m_rgchAchievementName;       // name of the achievement
+        public bool m_bAchieved;       // Is the icon for the achieved or not achieved version?
+        public int m_nIconHandle; // Handle to the image, which can be used in ClientUtils()->GetImageRGBA(), 0 means no image is set for the achievement
+
+        #region ICallbackData
+        public CallbackType Type => k_iCallback;
+        #endregion
+    }
+}
